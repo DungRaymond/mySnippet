a45c655340aa0ca8c81306f7d2e964e528efeee9
yvesNg <45696769+DungRaymond@users.noreply.github.com>
2022-11-16 14:10:55 +0700

    Create lott.js

---
+++ lott.js
@@ -0,0 +1,18 @@
+// 114
+// 115
+// 116
+// 117
+// 118
+// 119
+
+// gia tri cac bong = span[114] -> span[119]
+let bongtron = document.getElementsByTagName('span');
+for(i = 0; i < 6;i++) {
+  res.push(d[144 + i])
+};
+
+let chonkyquay = document.getElementsByTagName('select')[1];
+chonkyquay.options[966].selected = true;
+let doso = document.getElementsByClassName('choi_ngay')[13];
+let ngayquay = document.getElementsByTagName('td')[0];
+let kyquay = document.getElementsByTagName('a')[97];
