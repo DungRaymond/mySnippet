5655c8005d92e6df4fdfa3bfa1dfad2b1b6cdf0b
yvesNg <45696769+DungRaymond@users.noreply.github.com>
2022-11-18 17:02:12 +0700

    Update lott.js

--- lott.js
+++ lott.js
@@ -6,13 +6,34 @@
 // 119
 
 // gia tri cac bong = span[114] -> span[119]
-let bongtron = document.getElementsByTagName('span');
-for(i = 0; i < 6;i++) {
-  res.push(d[144 + i])
-};
+
 
 let chonkyquay = document.getElementsByTagName('select')[1];
-chonkyquay.options[966].selected = true;
 let doso = document.getElementsByClassName('choi_ngay')[13];
 let ngayquay = document.getElementsByTagName('td')[0];
 let kyquay = document.getElementsByTagName('a')[97];
+let bongtron = document.getElementsByTagName('span');
+let arr = [];
+let lottloop = () => {
+  for(let i = 969; i > 0; i--) {
+    chonkyquay.options[i].selected = true;
+    doso.click();
+    let result = {
+      date : ngayquay.innerHTML,
+      term: kyquay.innerHTML,
+      ketqua: [
+        bongtron[114].innerHTML,
+        bongtron[115].innerHTML,
+        bongtron[116].innerHTML,
+        bongtron[117].innerHTML,
+        bongtron[118].innerHTML,
+        bongtron[119].innerHTML
+      ]
+    };
+  arr.push(result)
+  setTimeout(() => {
+    
+  }, 500);
+
+  }
+}
