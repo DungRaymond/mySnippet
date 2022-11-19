8460941d79bffce69deaef2918e318dbc114869b
yvesNg <45696769+DungRaymond@users.noreply.github.com>
2022-11-19 14:57:14 +0700

    working good

--- lott.js
+++ lott.js
@@ -1,23 +1,21 @@
-// 114
-// 115
-// 116
-// 117
-// 118
-// 119
-
-// gia tri cac bong = span[114] -> span[119]
-
-
-let chonkyquay = document.getElementsByTagName('select')[1];
-let doso = document.getElementsByClassName('choi_ngay')[13];
-let ngayquay = document.getElementsByTagName('td')[0];
-let kyquay = document.getElementsByTagName('a')[97];
-let bongtron = document.getElementsByTagName('span');
 let arr = [];
-let lottloop = () => {
-  for(let i = 969; i > 0; i--) {
+function waitUI() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 500);
+  });
+}
+async function lottloop() {
+  for(let i = 1; i < 969; i++) {
+    let chonkyquay = document.getElementsByTagName('select')[1];
+    let doso = document.getElementsByClassName('choi_ngay')[13];
     chonkyquay.options[i].selected = true;
     doso.click();
+    const waitres = await waitUI();
+    let ngayquay = document.getElementsByTagName('td')[0];
+    let kyquay = document.getElementsByTagName('a')[97];
+    let bongtron = document.getElementsByTagName('span');
     let result = {
       date : ngayquay.innerHTML,
       term: kyquay.innerHTML,
@@ -30,10 +28,8 @@ let lottloop = () => {
         bongtron[119].innerHTML
       ]
     };
-  arr.push(result)
-  setTimeout(() => {
-    
-  }, 500);
+    console.log(kyquay.innerHTML)
+    arr.push(result)
 
   }
 }
